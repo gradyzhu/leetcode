98dee45dbfc6315b7128b8c2895b2e15a22eaa7a
your name here <gradyzhoo@gmail.com>
2019-05-08 20:27:11 -0700

    Add Product of Array Except Self

---
+++ product_of_array_except_self.js
@@ -0,0 +1,27 @@
+// leetcode #238
+
+// Given an array nums of n integers where n > 1,  return an array output such that output[i] is equal to the product of all the elements of nums except nums[i].
+
+// O(n) solution
+
+var productExceptSelf = function(nums) {
+  let res = [];
+  let left = [1];
+  let right = [];
+  
+  for (let i = 1; i < nums.length; i++) {
+      left[i] = left[i-1] * nums[i-1]
+  }
+  
+  for (let i = nums.length - 1; i >= 0; i--) {
+      if (i === nums.length - 1) {
+          right[i] = 1;
+          res[i] = right[i] * left[i];
+          continue;
+      }
+      right[i] = right[i+1] * nums[i+1];
+      res[i] = right[i] * left[i];
+  }
+  
+  return res;
+};

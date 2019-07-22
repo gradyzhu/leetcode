da3cfd77f62a6a62aae0695e878f01490ca8a351
your name here <gradyzhoo@gmail.com>
2019-07-22 13:01:43 -0700

    Add Longest Increasing Subsequence

---
+++ longest_increasing_subsequence.js
@@ -0,0 +1,24 @@
+// 300. Longest Increasing Subsequence
+
+var lengthOfLIS = function(nums) {
+  if (!nums.length) return [];
+  let dp = new Array(nums.length).fill(1);
+  let i = 1;
+  
+  while (i < nums.length) {
+      let currEnd = nums[i];
+      let j = 0;
+      while (j < i) {
+          let curr = nums[j];
+          if (nums[j] < nums[i]) {
+              dp[i] = Math.max(dp[i], dp[j] + 1)
+          }
+          j++;
+      }
+      i++;
+  }
+
+  return Math.max(...dp);
+};
+
+

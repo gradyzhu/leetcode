bf45ba16e433358cff26f887994cbc9144e215c4
your name here <gradyzhoo@gmail.com>
2019-07-24 18:57:09 -0700

    Add Palindrome Linked List

---
+++ palindrome_linked_list.js
@@ -0,0 +1,42 @@
+// 234. Palindrome Linked List
+
+var isPalindrome = function(head) {
+  if (!head) return true;
+  let mid = findMid(head);
+  let reversed = reverseList(mid);
+  
+  while (reversed) {
+      if (head.val !== reversed.val) return false;
+      head = head.next;
+      reversed = reversed.next;
+  }
+      
+  return true;
+};
+
+const reverseList = head => {
+  if (!head.next) return head;
+  let node = head, previous = null;
+
+  while (node) {
+      let tmp = node.next;
+      node.next = previous;
+      previous = node;
+      node = tmp;
+  }
+  return previous;
+}
+
+const findMid = head => {
+  let slow = head;
+  let fast = head;
+  let count = 0;
+  
+  while (fast && fast.next) {
+      slow = slow.next
+      fast = fast.next.next;
+      count++;
+  }
+  
+  return slow;
+}

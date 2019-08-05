f1125a52be1bdb7673512a74418e12e170a14a23
your name here <gradyzhoo@gmail.com>
2019-08-05 11:56:14 -0700

    Add Last Stone Weight

---
+++ last)stone_weight.js
@@ -0,0 +1,20 @@
+// leetcode 1046;
+
+var lastStoneWeight = function(stones) {
+  stones = stones.sort((a,b) => b - a);
+  
+  while (stones.length > 1) {
+      let largest = stones.shift();
+      let secondLargest = stones[0];
+
+      if (largest > secondLargest) {
+          stones[0] = largest - secondLargest;
+          stones = stones.sort((a,b) => b - a);
+      } else if (largest == secondLargest) {
+          stones.shift();
+      }
+  }
+  
+  console.log(stones)
+  return stones.length ? stones[0] : stones.length;
+};
